f77f9ed729d33810a492eed8d1a0666e7cd87086
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:51:56 +0100

    commit zero

---
+++ Among Us Script pastebin roblox Among Us v2021.4.14s .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+Among Us v2021.4.14s | NEW Version Among us is the latest at the moment. It is currently unknown
+]]
